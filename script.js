5a158f5240377a5ca227b24d285a656ba0e14fb2
Shiva <63032037+shivaakira12@users.noreply.github.com>
2024-06-01 10:55:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,11 +14,38 @@ document.querySelector("#close-contact-info").onclick = () => {
   contactinfo.classList.remove("active");
 };
 
+// var swiper = new Swiper(".home-slider", {
+//   // loop: true,
+//   // grabCursor: true,
+//   navigation: {
+//     nextE1: ".swiper-button-next",
+//     prevE1: ".swiper-button-prev",
+//   },
+// });
 var swiper = new Swiper(".home-slider", {
   loop: true,
   grabCursor: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".logo-slider", {
+  loop: true,
+  grabCursor: true,
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+    },
+    640: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1000: {
+      slidesPerView: 5,
+    },
   },
 });
